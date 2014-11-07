167e56bb24d0a851b0a9976ee734e90c60390ced
maasencioh <maasencioh@gmail.com>
2014-11-07 15:04:33 -0500

    fixing function test

--- test/dist/manhattan.js
+++ test/dist/manhattan.js
@@ -3,14 +3,14 @@ var distance = require('../..');
 var v1 = [0, 1, 4, 6, 2];
 var v2 = [3, 6, 9, 4, 3];
 
-describe('Squared euclidean distance', function () {
+describe('City block distance', function () {
 
     it('should return 0 with itself', function () {
-        distance.squaredEuclidean(v1, v1).should.equal(0);
+        distance.manhattan(v1, v1).should.equal(0);
     });
 
     it('should be correct', function () {
-        distance.squaredEuclidean(v1, v2).should.equal(16);
+        distance.manhattan(v1, v2).should.equal(16);
     });
 
 });
